85ba17062e76e34f4e0b3892b9ef89f220af5e7d
sultonovfazliddin09 <newgithub09@gmail.com>
2025-08-30 17:31:48 +0500

    news

--- app/(browse)/_components/navigation.tsx
+++ app/(browse)/_components/navigation.tsx
@@ -1,7 +1,36 @@
+import { navigation_links } from "@/app/constants";
+import {
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
 import React from "react";
 
 const Navigation = () => {
-  return <>Navigation</>;
+  return (
+    <>
+      <SidebarGroup />
+      <SidebarGroupLabel>Pages</SidebarGroupLabel>
+      <SidebarContent>
+        <SidebarMenu>
+          {navigation_links.map((item) => (
+            <SidebarMenuItem key={item.route}>
+              <SidebarMenuButton asChild>
+                <Link href={item.route}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarContent>
+    </>
+  );
 };
 
 export default Navigation;
