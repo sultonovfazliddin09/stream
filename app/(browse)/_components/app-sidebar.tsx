5ef9511936c51f87f0f37d0ed8ba284673f8a773
sultonovfazliddin09 <newgithub09@gmail.com>
2025-08-30 17:16:52 +0500

    new

--- app/(browse)/_components/app-sidebar.tsx
+++ app/(browse)/_components/app-sidebar.tsx
@@ -14,6 +14,7 @@ export function AppSidebar() {
         <Navigation />
         <SidebarSeparator />
         <Following />
+        <SidebarSeparator />
         <Recommended />
       </SidebarContent>
     </Sidebar>
